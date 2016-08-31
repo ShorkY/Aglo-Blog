d8c217492703bce1c94c9b4dd52e186e401a6797
PC <PC@PC-PC>
2016-08-31 23:33:20 +0300

    Add project files.

---
+++ AGLO-Blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AGLO_Blog.Startup))]
+namespace AGLO_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
